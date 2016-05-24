ab843710111b81528d36df49028e5094d4368e77
Marek4f <marekegg94@gmail.com>
2016-05-24 17:04:32 +0200

    utworzenie projektu
    Signed-off-by: marek.eggert <marek.eggert@itme.eu>

---
+++ App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(linkdownloader.Startup))]
+namespace linkdownloader
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
